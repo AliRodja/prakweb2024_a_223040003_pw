b62b0d06cb17b93d726510f7449cfb72a1a97350
AliRodja <rodjaali@gmail.com>
2024-11-01 11:26:35 +0700

    Video 7

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About</h1>
+<div class="container">
+    <h1 class="mt-4">Tentang Saya</h1>
+    <img src="<?= BASEURL; ?>/img/profilesaya.jpeg" alt="" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?=$data['pekerjaan']; ?>, saya berumur <?= $data['umur']; ?> tahun</p>
+</div>
